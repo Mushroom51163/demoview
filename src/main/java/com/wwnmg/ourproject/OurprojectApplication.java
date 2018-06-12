69520bf67b3d33d63f73b6c0d51fb0da9c019bab
Jim <wangl_1995@foxmail.com>
2018-06-12 09:45:05 +0800

    first commit

---
+++ src/main/java/com/wwnmg/ourproject/OurprojectApplication.java
@@ -0,0 +1,12 @@
+package com.wwnmg.ourproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurprojectApplication.class, args);
+    }
+}
